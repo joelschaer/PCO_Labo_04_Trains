7ef9c7e8e95bfbe5ccfc232b0114cacbc1d398ea
joelschar <joel.schar@heig-vd.ch>
2018-04-20 15:21:32 +0200

    éguillages

--- QtrainSim/src/student_cpp/chefquai.h
+++ QtrainSim/src/student_cpp/chefquai.h
@@ -25,36 +25,131 @@ public:
     }
 
     void regler_aiguillage(int numeroTrain, int nextPoint){
-        if(numeroTrain == 0){
             switch(nextPoint){
-            case 15:
-                diriger_aiguillage(8, DEVIE,  0);
-                break;
-            case 6:
-                diriger_aiguillage(2, DEVIE,  0);
-                break;
-            case 33:
-                diriger_aiguillage(20, DEVIE,  0);
-                break;
-            case 24:
-                diriger_aiguillage(14, DEVIE,  0);
-                break;
-            }
-        } else if(numeroTrain == 1){
-            switch(nextPoint){
-            case 15:
-                diriger_aiguillage(8, DEVIE,  0);
-                break;
-            case 6:
+            case 1:
                 diriger_aiguillage(2, TOUT_DROIT,  0);
                 break;
-            case 33:
-                diriger_aiguillage(20, DEVIE,  0);
+            case 2:
+                diriger_aiguillage(1, DEVIE,  0);
                 break;
-            case 24:
+            case 3:
+                diriger_aiguillage(4, TOUT_DROIT,  0);
+                break;
+            case 4:
+                diriger_aiguillage(3, DEVIE,  0);
+                break;
+            case 5:
+                diriger_aiguillage(2, DEVIE,  0);
+                break;
+            case 6:
+                diriger_aiguillage(1, TOUT_DROIT,  0);
+                diriger_aiguillage(4, DEVIE,  0);
+                break;
+            case 7:
+                diriger_aiguillage(3, DEVIE,  0);
+                diriger_aiguillage(5, TOUT_DROIT,  0);
+                break;
+            case 8:
+                diriger_aiguillage(3, TOUT_DROIT,  0);
+                diriger_aiguillage(5, DEVIE,  0);
+                break;
+            case 9:
+                diriger_aiguillage(18, DEVIE,  0);
+                diriger_aiguillage(24, TOUT_DROIT,  0);
+                break;
+            case 10:
+                diriger_aiguillage(8, TOUT_DROIT,  0);
+                break;
+            case 11:
+                diriger_aiguillage(7, TOUT_DROIT,  0);
+                break;
+            case 12:
+                diriger_aiguillage(10, TOUT_DROIT,  0);
+                break;
+            case 13:
+                diriger_aiguillage(9, TOUT_DROIT,  0);
+                break;
+            case 14:
+                diriger_aiguillage(8, DEVIE,  0);
+                diriger_aiguillage(11, TOUT_DROIT,  0);
+                break;
+            case 15:
+                diriger_aiguillage(7, DEVIE,  0);
+                diriger_aiguillage(10, DEVIE,  0);
+                break;
+            case 16:
+                diriger_aiguillage(9, DEVIE,  0);
+                break;
+            case 17:
+                diriger_aiguillage(18, TOUT_DROIT,  0);
+                diriger_aiguillage(24, DEVIE,  0);
+                break;
+            case 18:
+                diriger_aiguillage(8, DEVIE,  0);
+                diriger_aiguillage(11, DEVIE,  0);
+                break;
+            case 19:
                 diriger_aiguillage(14, TOUT_DROIT,  0);
                 break;
-            }
+            case 20:
+                diriger_aiguillage(13, TOUT_DROIT,  0);
+                break;
+            case 21:
+                diriger_aiguillage(16, TOUT_DROIT,  0);
+                break;
+            case 22:
+                diriger_aiguillage(15, TOUT_DROIT,  0);
+                break;
+            case 23:
+                diriger_aiguillage(14, DEVIE,  0);
+                break;
+            case 24:
+                diriger_aiguillage(13, DEVIE,  0);
+                diriger_aiguillage(16, DEVIE,  0);
+                break;
+            case 25:
+                diriger_aiguillage(15, DEVIE,  0);
+                diriger_aiguillage(17, TOUT_DROIT,  0);
+                break;
+            case 26:
+                diriger_aiguillage(15, DEVIE,  0);
+                diriger_aiguillage(17, DEVIE,  0);
+                break;
+            case 27:
+                diriger_aiguillage(6, DEVIE, 0);
+                diriger_aiguillage(12, TOUT_DROIT,  0);
+                break;
+            case 28:
+                diriger_aiguillage(20, TOUT_DROIT,  0);
+                break;
+            case 29:
+                diriger_aiguillage(19, TOUT_DROIT,  0);
+                break;
+            case 30:
+                diriger_aiguillage(22, TOUT_DROIT,  0);
+                break;
+            case 31:
+                diriger_aiguillage(21, TOUT_DROIT,  0);
+                break;
+            case 32:
+                diriger_aiguillage(20, DEVIE,  0);
+                diriger_aiguillage(23, TOUT_DROIT,  0);
+                break;
+            case 33:
+                diriger_aiguillage(19, DEVIE,  0);
+                diriger_aiguillage(22, DEVIE,  0);
+                break;
+            case 34:
+                diriger_aiguillage(21, DEVIE,  0);
+                break;
+            case 35:
+                diriger_aiguillage(6, TOUT_DROIT,  0);
+                diriger_aiguillage(12, DEVIE,  0);
+                break;
+            case 36:
+                diriger_aiguillage(20, DEVIE,  0);
+                diriger_aiguillage(23, DEVIE,  0);
+                break;
         }
     }
 
--- QtrainSim/src/student_cpp/cheftrain.h
+++ QtrainSim/src/student_cpp/cheftrain.h
@@ -25,35 +25,73 @@ public:
 
     void run()Q_DECL_OVERRIDE{
         //Attente du passage sur les contacts
+        int nbTours = 0;
+        int sens = 1;
         while(true){
-        for (int i = 0; i < parcours->size(); i++) {
+            if(nbTours == 2){
+                loco->inverserSens();
+                sens *= -1;
+                nbTours = 0;
+            }
 
-            bool ok = chef->isDispo(loco->numero(),parcours->at(i));
-                if(ok){
-                    loco->demarrer();
-                    chef->regler_aiguillage(loco->numero(), parcours->at(i));
-                    attendre_contact(parcours->at(i));
-                    chef->changeSegment(loco->numero(), parcours->at(i));
+            if(sens == 1){
+                for (int i = 0; i < parcours->size(); i++) {
 
-                    afficher_message(qPrintable(QString("The engine no. ").append(loco->numero()).append(" has reached contact no. %2.")
-                                                .arg(loco->numero()).arg(parcours->at(i))));
-                    loco->afficherMessage(QString("I've reached contact no. %1.").arg(parcours->at(i)));
-                }
-                else{
-                    afficher_message(qPrintable(QString("stop")));
-                    loco->arreter();
-                    while(true){
-                        bool ok = chef->isDispo(loco->numero(),parcours->at(i));
-                        if(ok){
-                            break;
+                    bool ok = chef->isDispo(loco->numero(),parcours->at(i));
+                    if(ok){
+                        loco->demarrer();
+                        chef->regler_aiguillage(loco->numero(), parcours->at(i));
+                        attendre_contact(parcours->at(i));
+                        chef->changeSegment(loco->numero(), parcours->at(i));
+
+                        afficher_message(qPrintable(QString("The engine no. ").append(loco->numero()).append(" has reached contact no. %2.")
+                                                    .arg(loco->numero()).arg(parcours->at(i))));
+                        loco->afficherMessage(QString("I've reached contact no. %1.").arg(parcours->at(i)));
+                    }
+                    else{
+                        afficher_message(qPrintable(QString("stop")));
+                        loco->arreter();
+                        while(true){
+                            bool ok = chef->isDispo(loco->numero(),parcours->at(i));
+                            if(ok){
+                                break;
+                            }
                         }
                     }
                 }
             }
-        }
-        // lorsque contacte connu
-        // demande au chef de quai si peut continuer ou pas
+            else{
+                for (int i = parcours->size()-1 ; i >= 0; i--) {
+
+                    bool ok = chef->isDispo(loco->numero(),parcours->at(i));
+                    if(ok){
+                        loco->demarrer();
+                        chef->regler_aiguillage(loco->numero(), parcours->at(i));
+                        attendre_contact(parcours->at(i));
+                        chef->changeSegment(loco->numero(), parcours->at(i));
+
+                        afficher_message(qPrintable(QString("The engine no. ").append(loco->numero()).append(" has reached contact no. %2.")
+                                                    .arg(loco->numero()).arg(parcours->at(i))));
+                        loco->afficherMessage(QString("I've reached contact no. %1.").arg(parcours->at(i)));
+                    }
+                    else{
+                        afficher_message(qPrintable(QString("stop")));
+                        loco->arreter();
+                        while(true){
+                            bool ok = chef->isDispo(loco->numero(),parcours->at(i));
+                            if(ok){
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+        nbTours++;
     }
+    // lorsque contacte connu
+    // demande au chef de quai si peut continuer ou pas
+}
 
 };
 
