8660272177e8add0bf065326f6bccd333c06d659
joelschar <joel.schar@heig-vd.ch>
2018-04-23 20:49:26 +0200

    fonctionne avec prio pour la loco 1 et sans prio ( les deux loco ont une priorite de 0)

--- QtrainSim/src/student_cpp/cheftrain.h
+++ QtrainSim/src/student_cpp/cheftrain.h
@@ -13,95 +13,167 @@ private:
     Locomotive *loco;
     ChefQuai *chef;
     QList<int> *parcours;
+    QList<int> *parcours_inv;
+    int sensDepart;
+    int priorite; // 0: pas de prio / 1: prioritaire
+    QList<int> *deviation1;
+    QList<int> *deviation1_inv;
+    QList<int> *deviation2;
+    QList<int> *deviation2_inv;
+    int deviationLength;
+
 
 public:
     ChefTrain(){}
 
-    ChefTrain(Locomotive *loco, QList<int>* parcours, ChefQuai* chef){
+    ChefTrain(Locomotive *loco, QList<int>* parcours, ChefQuai* chef, int sensDepart, int priorite){
         this->loco = loco;
         this->parcours = parcours;
         this->chef = chef;
+        this->sensDepart = sensDepart;
+        this->priorite = priorite;
+
+        this->parcours_inv = inverse_qlist(parcours);
     }
 
     void run()Q_DECL_OVERRIDE{
         //Attente du passage sur les contacts
         int nbTours = 0;
         int sens = 1;
-        bool ok;
+
         while(true){
             if(nbTours == 2){
+                loco->arreter();
+                sleep(1);
                 loco->inverserSens();
                 sens *= -1;
                 nbTours = 0;
             }
 
+        if(loco->numero() == 1)
+            printf("hello");
+
+
             if(sens == 1){
-                for (int i = 0; i < parcours->size(); i++) {
-                    if(i == parcours->size()-1)
-                            printf("hello");
-
-                    ok = chef->isDispo(loco->numero(),parcours->at(i), sens);
-                    if(i < parcours->size()-1){
-                        ok &= chef->isDispo(loco->numero(),parcours->at(i+1), sens);
-                    }
-                    if(ok){
-                        loco->demarrer();
-                        chef->regler_aiguillage(loco->numero(), parcours->at(i));
-                        attendre_contact(parcours->at(i));
-                        chef->changeSegment(loco->numero(), parcours->at(i), sens);
-
-                        afficher_message(qPrintable(QString("The engine no. ").append(loco->numero()).append(" has reached contact no. %2.")
-                                                    .arg(loco->numero()).arg(parcours->at(i))));
-                        loco->afficherMessage(QString("I've reached contact no. %1.").arg(parcours->at(i)));
-                    }
-                    else{
-                        afficher_message(qPrintable(QString("stop")));
-                        loco->arreter();
-                        while(true){
-                            bool ok = chef->isDispo(loco->numero(),parcours->at(i), sens);
-                            if(ok){
-                                break;
-                            }
-                        }
-                    }
-                }
+                runParcours(parcours, deviation1, deviation2, sens);
             }
+            //sens inverse (-1)
             else{
-                for (int i = parcours->size()-1 ; i >= 0; i--) {
-
-                    ok = chef->isDispo(loco->numero(),parcours->at(i), sens);
-                    if(i > 0){
-                        ok &= chef->isDispo(loco->numero(),parcours->at(i-1), sens);
-                    }
-                    if(ok){
-                        loco->demarrer();
-                        chef->regler_aiguillage(loco->numero(), parcours->at(i));
-                        attendre_contact(parcours->at(i));
-                        chef->changeSegment(loco->numero(), parcours->at(i), sens);
-
-                        afficher_message(qPrintable(QString("The engine no. ").append(loco->numero()).append(" has reached contact no. %2.")
-                                                    .arg(loco->numero()).arg(parcours->at(i))));
-                        loco->afficherMessage(QString("I've reached contact no. %1.").arg(parcours->at(i)));
-                    }
-                    else{
-                        afficher_message(qPrintable(QString("stop")));
-                        loco->arreter();
-                        while(true){
-                            bool ok = chef->isDispo(loco->numero(),parcours->at(i), sens);
-                            if(ok){
-                                break;
-                            }
-                        }
-                    }
-                }
+                runParcours(parcours_inv, deviation1_inv, deviation2_inv, sens);
             }
-
             nbTours++;
         }
         // lorsque contacte connu
         // demande au chef de quai si peut continuer ou pas
     }
 
-};
+    void runParcours(QList<int> *parcours, QList<int> *deviation1, QList<int> *deviation2, int sens){
+        bool libre;
+
+        for (int i = 0; i < parcours->size(); i++) {
+
+            // test si la prochaine et la surprochaine section est libre
+            libre = chef->isDispo(loco->numero(),parcours->at(i), sens);
+            if(i < parcours->size()-1){
+                libre &= chef->isDispo(loco->numero(),parcours->at(i+1), sens);
+            }
+
+            if(libre){
+                loco->demarrer();
+                chef->regler_aiguillage(loco->numero(), parcours->at(i), sens);
+                attendre_contact(parcours->at(i));
+                chef->changeSegment(loco->numero(), parcours->at(i), sens);
+
+                afficher_message(qPrintable(QString("The engine no. ").append(loco->numero()).append(" has reached contact no. %2.")
+                                            .arg(loco->numero()).arg(parcours->at(i))));
+                loco->afficherMessage(QString("I've reached contact no. %1.").arg(parcours->at(i)));
+            }
+            else{
+                // le train est prioritaire donc il prend la voie de déviation
+                if(priorite == 1){
+                    loco->afficherMessage(qPrintable(QString("train devié")));
+                    loco->demarrer();
+
+                    switch(parcours->at(i)){
+                    case 15:
+                        if(sens == 1){
+                            runDeviation(deviation1, sens);
+                            i = 5;
+                        }
+                        break;
+                    case 6:
+                        if(sens == -1){
+                            runDeviation(deviation1, sens);
+                            i = parcours->length()-1;
+                        }
+                        break;
+                    case 33:
+                        if(sens == 1){
+                            runDeviation(deviation2, sens);
+                            i = parcours->length()-1;
+                        }
+                        break;
+                    case 24:
+                        if(sens == -1){
+                            runDeviation(deviation2, sens);
+                            i = 5;
+                        }
+                        break;
+                    }
+                }
+
+                // le train n'est pas prioritaire, donc il s'arrête
+                else{
+                    loco->afficherMessage(qPrintable(QString("stop")));
+                    loco->arreter();
+                    sleep(1);
+
+                    // Quesiton pour le prof : si le train repart d'ici, il y aura incrément de i et donc on saute l'attente d'un contact
+                    while(true){
+                        bool ok = chef->isDispo(loco->numero(),parcours->at(i), sens);
+                        ok &= chef->isDispo(loco->numero(),parcours->at(i+1), sens);
+                        if(ok){
+                            sleep(1);
+                            break;
+                        }
+                    }
+                }
+            }
+        }// fin for
+    } // fin runParcours
+
+    void runDeviation(QList<int> *deviation, int sens){
+        for(int i = 0; i < deviation->length(); ++i){
+            chef->regler_aiguillage(loco->numero(), deviation->at(i), sens);
+            attendre_contact(deviation->at(i));
+            afficher_message(qPrintable(QString("The engine no. ").append(loco->numero()).append(" has reached contact no. %2.")
+                                        .arg(loco->numero()).arg(deviation->at(i))));
+            loco->afficherMessage(QString("I've reached contact no. %1.").arg(deviation->at(i)));
+        }
+    } // fin runDeviation
+
+    void setDev(int deviationLength, QList<int>* deviation1, QList<int>* deviation2){
+        this->deviationLength = deviationLength;
+        this->deviation1 = deviation1;
+        this->deviation1_inv = inverse_qlist(deviation1);
+        this->deviation2 = deviation2;
+        this->deviation2_inv = inverse_qlist(deviation2);
+    } // fin setDeviation
+
+    QList<int>* inverse_qlist(QList<int>* list){
+        QList<int>* qlist = new QList<int>();
+
+        for(int i = list->length()-1; i >= 0; i--){
+            qlist->append(list->at(i));
+        }
+        return qlist;
+    }
+
+    ~ChefTrain(){
+        delete parcours;
+        delete deviation1_inv;
+        delete deviation2_inv;
+    }
+}; // fin ChefTrain
 
 #endif // CHEFTRAIN_H
--- QtrainSim/src/student_cpp/cppmain.cpp
+++ QtrainSim/src/student_cpp/cppmain.cpp
@@ -33,6 +33,13 @@ int cmain()
     QList<int> parcours2;
     parcours2 << 13 << 15 << 14 << 7 << 6 << 1 << 31 << 33 <<  32 << 25 << 24 << 19;
 
+    //parcours de deviations
+    QList<int> deviation1;
+    deviation1 << 13 << 12 << 11 << 10 << 4 << 3 << 2 << 1;
+
+    QList<int> deviation2;
+    deviation2 << 31 << 30 << 29 << 28 << 22 << 21 << 20 << 19;
+
     //Initialisation des aiguillages
     diriger_aiguillage(8,  DEVIE,       0);
     diriger_aiguillage(2,  DEVIE,       0);
@@ -44,7 +51,7 @@ int cmain()
 
     //Initialisation de la locomotive
     locomotive.fixerNumero(0);
-    locomotive.fixerVitesse(12);
+    locomotive.fixerVitesse(17);
     locomotive.fixerPosition(16, 23);
     locomotive.allumerPhares();
     //locomotive.demarrer();
@@ -52,41 +59,21 @@ int cmain()
 
     //Initialisation de la locomotive 2
     locomotive2.fixerNumero(1);
-    locomotive2.fixerVitesse(15);
+    locomotive2.fixerVitesse(14);
     locomotive2.fixerPosition(13, 19);
     locomotive2.allumerPhares();
     //locomotive2.demarrer();
     locomotive2.afficherMessage("Ready!");
 
 
+    // il suffit de mettre la priorité des deux locomotives à 0 pour supprimer l'utilisation de la voie d'évitement.
     ChefQuai* chefQuai = new ChefQuai();
-    ChefTrain* chefTrain1 = new ChefTrain(&locomotive, &parcours, chefQuai);
-    ChefTrain* chefTrain2 = new ChefTrain(&locomotive2, &parcours2, chefQuai);
+    ChefTrain* chefTrain1 = new ChefTrain(&locomotive, &parcours, chefQuai, 1, 0);
+    ChefTrain* chefTrain2 = new ChefTrain(&locomotive2, &parcours2, chefQuai, 1, 1);
+    chefTrain2->setDev(8, &deviation1, &deviation2);
     chefTrain1->start();
     chefTrain2->start();
 
-
-    //Attente du passage sur les contacts
-    /*
-    for (int i = 0; i < parcours.size(); i++) {
-        attendre_contact(parcours.at(i));
-        afficher_message(qPrintable(QString("The engine no. %1 has reached contact no. %2.")
-                                    .arg(locomotive.numero()).arg(parcours.at(i))));
-        locomotive.afficherMessage(QString("I've reached contact no. %1.").arg(parcours.at(i)));
-    }
-
-    //Arreter la locomotive
-    locomotive.arreter();
-    locomotive.afficherMessage("Yeah, piece of cake!");
-
-    //Fin de la simulation
-    mettre_maquette_hors_service();
-
-    //Exemple de commande
-    afficher_message("Enter a command in the input field at the top of the window.");
-    QString commande = getCommand();
-    afficher_message(qPrintable(QString("Your command is: ") + commande));
-    */
     chefTrain1->wait();
     chefTrain2->wait();
     return EXIT_SUCCESS;
